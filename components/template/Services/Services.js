fbc966fff2cdafa82524edc3882e78f6e09d730a
mohammadmehdi <mohammad333mehdi@gmail.com>
2023-12-13 17:32:37 +0330

    create section services part-2 end

--- components/template/Services/Services.js
+++ components/template/Services/Services.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import style from '@/styles/services.module.css'
 import Title from '@/components/modules/Title/Title'
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false;
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCaretUp, faComputer, faPerson, faPersonArrowUpFromLine } from "@fortawesome/free-solid-svg-icons";
 
 export default function Services() {
   return (
@@ -37,6 +42,45 @@ export default function Services() {
                     <h5 className={style.services_item_title}>آنالیزکسب و کار</h5>
                     <p className={style.services_item_desc}>جلسه زمانی است که دو یا چند نفر گرد هم می آیند تا درباره یک یا چند موضوع اغلب در یک جلسه رسمی یا تجاری بحث کنند</p>
                     </div>
+                </div>
+            </div>
+        </div>
+
+        <div className="row mt-5">
+            <div className="col-lg-3 col-md-6 col-sm-12">
+                <div className={style.services_card}>
+                    <span className={style.services_card_icon}>
+                        <FontAwesomeIcon icon={faPersonArrowUpFromLine}/>
+                    </span>
+                    <span className={style.services_card_number}>999</span>
+                    <span className={style.services_card_text}>مشتری راضی</span>
+                </div>
+            </div>
+            <div className="col-lg-3 col-md-6 col-sm-12">
+                <div className={style.services_card}>
+                    <span className={style.services_card_icon}>
+                        <FontAwesomeIcon icon={faComputer}/>
+                    </span>
+                    <span className={style.services_card_number}>185</span>
+                    <span className={style.services_card_text}>وبسایت ساهت شده</span>
+                </div>
+            </div>
+            <div className="col-lg-3 col-md-6 col-sm-12">
+                <div className={style.services_card}>
+                    <span className={style.services_card_icon}>
+                        <FontAwesomeIcon icon={faPerson}/>
+                    </span>
+                    <span className={style.services_card_number}>100</span>
+                    <span className={style.services_card_text}>متخصص</span>
+                </div>
+            </div>
+            <div className="col-lg-3 col-md-6 col-sm-12">
+                <div className={style.services_card}>
+                    <span className={style.services_card_icon}>
+                        <FontAwesomeIcon icon={faCaretUp}/>
+                    </span>
+                    <span className={style.services_card_number}>20</span>
+                    <span className={style.services_card_text}>سال تجربه</span>
                 </div>
             </div>
         </div>
